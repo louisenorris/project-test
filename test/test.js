1b7d2b650bfc100ddf82338ff9c8b6aab9b7db20
Louise Norris <louisenorris73@yahoo.co.uk>
2020-01-16 17:11:05 +0000

    wrote first test using mocha and chai

---
+++ test/test.js
@@ -0,0 +1,34 @@
+const should = require('chai').should();
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
+const Add = require('../index');
+
+describe('Addition Tests', function(){
+    it('should return 3 when passed one and two', function() {
+        let numberOne = 1;
+        let numberTwo = 2;
+
+        let expectedResult = 3;
+
+        const actualResult = Add(numberOne, numberTwo)
+
+        // actualResult.should.equal(expectedResult)
+        // expect(actualResult).to.equal(expectedResult)
+        assert.equal(actualResult, expectedResult, 'error: should return 3 when passed one and two')
+    })
+
+    it('should not return 3 when passed one and four', function() {
+        let numberOne = 1;
+        let numberTwo = 4;
+
+        let notExpectedResult = 3;
+
+        const actualResult = Add(numberOne, numberTwo)
+
+        // actualResult.should.not.equal(notExpectedResult)
+        // expect(actualResult).to.not.equal(notExpectedResult)
+        assert.notEqual(actualResult, notExpectedResult, 'error: should not return 3 when passed one and four')
+
+    })
+})
